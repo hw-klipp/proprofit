c7e9bad224e1ca5c4ecf8115113111d65665151c
hw-klipp <drug.sinoptik@gmail.com>
2018-06-27 21:56:09 +0300

    fixed bug

--- index.php
+++ index.php
@@ -3,11 +3,4 @@ require_once __DIR__ . '/app/config/init.php';
 
 $newsPage = new \app\core\View();
 $newsPage->news = \app\models\Article::latestNews();
-$newsPage->asd = 'asd';
-
-foreach ($newsPage as $k => $v) {
-    var_dump($k);
-}
-
-die;
 $newsPage->display(VIEW . 'news.php');
